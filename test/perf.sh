35e34e81abbed8ed611cabf06e2e64899d6e5ded
Nir Soffer <nirsof@gmail.com>
2025-02-16 15:52:20 +0200

    Improve performance test
    - Create N vms (default 2) to allow testing scaling issues
    - Run tests with N vms (default 2)
    - Run all tests with same vms, instead of starting an stopping the vms
      for each test
    - Add vm-to-host test (iperf3 -c server --reverse)
    - Add -v option to set the vm type
    - Add -b option to test bridged mode
    - Store test results in JSON format
    - Add command line options instead of positional arguments
    - Rename to vms so they are less likely to clash with real vms
    - Minimize vm memory
    - Improve logging
    - Add test/README.md documenting perf.sh usage
    
    Signed-off-by: Nir Soffer <nirsof@gmail.com>

--- test/perf.sh
+++ test/perf.sh
@@ -1,76 +1,152 @@
 #!/bin/bash
+
 set -e
+set -o pipefail
+
 cd "$(dirname "$0")"
 
-TIME=${2:-30}
-
 server_address() {
-    limactl shell server ip -j -4 addr show dev lima0 | jq -r '.[0].addr_info[0].local'
+    limactl shell perf1 ip -j -4 addr show dev lima0 | jq -r '.[0].addr_info[0].local'
 }
 
 create() {
-    limactl create --name server --tty=false lima.yaml &
-    limactl create --name client --tty=false lima.yaml &
+    echo "[perf] Creating $count vms"
+    for i in $(seq $count); do
+        limactl create --name perf$i --tty=false lima.yaml &
+    done
     wait
-}
-
-host-to-vm() {
-    limactl start server
-    limactl shell server sudo systemctl start iperf3.service
-    iperf3-darwin --client $(server_address) --time $TIME
-    limactl stop server
-}
-
-host-to-vm-2() {
-    limactl start server &
-    limactl start client &
-    wait
-    limactl shell server sudo systemctl start iperf3.service
-    iperf3-darwin --client $(server_address) --time $TIME
-    limactl stop server
-    limactl stop client
-}
-
-vm-to-vm() {
-    limactl start server &
-    limactl start client &
-    wait
-    limactl shell server sudo systemctl start iperf3.service
-    addr=$(server_address)
-    limactl shell client iperf3 --client $addr --time $TIME
-    limactl stop server
-    limactl stop client
 }
 
 delete() {
-    limactl delete -f server
-    limactl delete -f client
+    echo "[perf] Delete $count vms"
+    for i in $(seq $count); do
+        limactl delete -f perf$i
+    done
 }
 
+run() {
+    echo "[perf] Updating vms"
+    update_vms
+
+    echo "[perf] Starting vms"
+    start_vms
+
+    echo "[perf] Starting iperf3 service"
+    limactl shell perf1 sudo systemctl start iperf3.service >>perf.log
+
+    config="$out_dir/$mode-$vm_type-$count"
+    mkdir -p "$config"
+    addr=$(server_address)
+
+    echo "[perf] Running $config/host-to-vm"
+    iperf3-darwin --client $addr --time $time --json > "$config/host-to-vm.json"
+
+    echo "[perf] Running $config/vm-to-host"
+    iperf3-darwin --client $addr --time $time --reverse --json > "$config/vm-to-host.json"
+
+    if [ $count -gt 1 ]; then
+        echo "[perf] Running $config/vm-to-vm"
+        limactl shell perf2 iperf3 --client $addr --time $time --json > "$config/vm-to-vm.json"
+    fi
+
+    echo "[perf] Stopping vms"
+    stop_vms
+}
+
+update_vms() {
+    socket="/var/run/socket_vmnet"
+    if [ "$mode" = "bridged" ]; then
+        socket+=".bridged.$iface"
+    fi
+    for i in $(seq $count); do
+        limactl edit perf$i --tty=false \
+            --set ".vmType = \"$vm_type\" | .networks[0].socket = \"$socket\""
+    done
+}
+
+start_vms() {
+    for i in $(seq $count); do
+        limactl start perf$i &
+    done
+    wait
+}
+
+stop_vms() {
+    for i in $(seq $count); do
+        limactl stop perf$i
+    done
+}
+
+usage() {
+    echo "Usage $0 command [options]"
+    echo
+    echo "Commands:"
+    echo "  create          create test vms"
+    echo "  delete          delete test vms"
+    echo "  run             run benchmarks"
+    echo
+    echo "Options:"
+    echo "  -c COUNT        number of vms"
+    echo "  -t SECONDS      time in seconds to transmit for (default 30)"
+    echo "  -v VM_TYPE      lima vmType [vz, qemu] (default vz)"
+    echo "  -b IFACE        if set, use lima bridged network with IFACE"
+    echo "  -o OUT_DIR      output directory (default perf.out)"
+}
+
+# Defaults
+count=2
+time=30
+vm_type=vz
+mode=shared
+iface=
+out_dir=perf.out
+
 case $1 in
-create)
-    create
-    ;;
-host-to-vm)
-    host-to-vm
-    ;;
-host-to-vm-2)
-    host-to-vm-2
-    ;;
-vm-to-vm)
-    vm-to-vm
-    ;;
-delete)
-    delete
+create|delete|run)
+    command=$1
+    shift
     ;;
 *)
-    echo "Usage $0 command"
-    echo
-    echo "Available commands:"
-    echo "  create                  create test vms"
-    echo "  delete                  delete test vms"
-    echo "  host-to-vm [TIME]       test host to vm performance"
-    echo "  host-to-vm-2 [TIME]     test host to vm performance with 1 extra vm"
-    echo "  vm-to-vm [TIME]         test vm to vm performance"
+    usage
+    exit 1
     ;;
 esac
+
+args=$(getopt c:t:v:b:o: $*)
+if [ $? -ne 0 ]; then
+    usage
+    exit 1
+fi
+
+set -- $args
+
+while :; do
+    case $1 in
+    -c)
+        count=$2
+        shift; shift
+        ;;
+    -t)
+        time=$2
+        shift; shift
+        ;;
+    -v)
+        vm_type="$2"
+        shift; shift
+        ;;
+    -b)
+        mode=bridged
+        iface="$2"
+        shift; shift
+        ;;
+    -o)
+        out_dir="$2"
+        shift; shift
+        ;;
+    --)
+        shift; break;
+        ;;
+    esac
+done
+
+$command
